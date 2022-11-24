579089a6b3b5ba78c4198f8aacaaa1a80cfc6e8e
Gokul <gokulgamer9404@gmail.com>
2022-11-24 19:41:00 +0530

    Removed a minor bug from use_case_example file

--- use_case_example.java
+++ use_case_example.java
@@ -11,17 +11,19 @@ import java.util.Scanner;
 public class use_case_example {
    public static void main(String[] args) {
       try {
+         int z = 0, Run = 1;
+         extras ets = new extras();
+         Scanner tb_sc = new Scanner(System.in);
          Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "gokul");
          Statement statement = conn.createStatement();
          
-         Scanner tb_sc = new Scanner(System.in);
-         int z = 0, a=1;
-         extras ets = new extras();
+         DML_Table tabel = new DML_Table(conn, statement);
          DDML_Database dbs = new DDML_Database(conn, statement);
+         
          System.out.println(":: Select a database to have some work ::");
          dbs.choose_connect_databases();
-         DML_Table tabel = new DML_Table(conn, statement);
-         while (a == 1) {
+         
+         while (Run == 1) {
             System.out.println(":: Select any operation to perform on database/table on database ::");
             System.out.println("1) Show table list");
             System.out.println("2) Descirbe any table on database");
@@ -35,7 +37,7 @@ public class use_case_example {
             while (true) {
                try {
                   z = tb_sc.nextInt();
-                  if(z>0 && z<9){
+                  if (z > 0 && z < 9) {
                      break;
                   }
                } catch (Exception e) {
@@ -47,43 +49,44 @@ public class use_case_example {
                case 1:
                   tabel.show_tb_list();
                   break;
-            
+
                case 2:
                   tabel.choose_describe_table();
                   break;
-            
+
                case 3:
                   tabel.choose_table_insert_values();
                   break;
-            
+
                case 4:
                   tabel.choose_show_table();
                   break;
-            
+
                case 5:
                   tabel.choose_show_specific_row();
                   break;
-            
+
                case 6:
                   tabel.choose_show_specific_column();
                   break;
-            
+
                case 7:
                   tabel.choose_delete_table();
                   break;
-            
+
                case 8:
-                  a = 0;
-                  break;
+                  Run--;
 
                default:
                   break;
             }
-            tb_sc.nextLine();
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
+            if (Run == 1) {
+               tb_sc.nextLine();
+               System.out.print("\033[H\033[2J");
+               System.out.flush();
+            }
          }
-         
+
          conn.close();
       } catch (SQLException e) {
          e.printStackTrace();
